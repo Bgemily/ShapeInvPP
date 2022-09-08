213039a4f47936544b83aa5b6297a111dac81a2f
Zitong Zhang <zztzhang@ucdavis.edu>
2022-09-08 00:57:34 -0400

    Simulate data with varying time shift variance. Apply FPCA to simulated data

--- Functions/generate_data.R
+++ Functions/generate_data.R
@@ -1,4 +1,4 @@
-### Generate synthetic networks
+### Generate synthetic point processes
 generate_data = function(SEED=NULL,
                          N_node=100, 
                          N_replicate=1,
@@ -8,6 +8,7 @@ generate_data = function(SEED=NULL,
                          t_vec = seq(-u_0,u_1,by=0.01),
                          t_vec_extend = t_vec,
                          N_spks_total = 1000,
+                         timeshift_max_vec = c(1/8, 1/32),
                          ### params when N_clus==4:
                          clus_sep = 2,
                          ### params when N_clus==1:
@@ -27,22 +28,16 @@ generate_data = function(SEED=NULL,
   mem_true_vec = rep(1:N_clus, clus_size_vec)
   clus_true_list = mem2clus(mem_true_vec, N_clus_min = N_clus)
   
-  
   # Generate time shifts ----------------------------------------------------
   v_mat_list = list()
   v_mat_list[[1]] = runif(n=N_node*N_replicate, 
-                          min = 0,
-                          max = u_0/2)  
+                          min = -timeshift_max_vec[1],
+                          max = timeshift_max_vec[1])  
   v_mat_list[[1]] = matrix(v_mat_list[[1]], nrow = N_node, ncol = N_replicate)
   v_mat_list[[2]] = runif(n = N_node*N_replicate,
-                          min = 0,
-                          max = u_1-u_0/2 )
+                          min = -timeshift_max_vec[2],
+                          max = timeshift_max_vec[2] )
   v_mat_list[[2]] = matrix(v_mat_list[[2]], nrow = N_node, ncol = N_replicate)
-  for(id_clus in 1:N_clus){
-    v_mat_list[[1]][clus_true_list[[id_clus]], ] = v_mat_list[[1]][clus_true_list[[id_clus]], ] - min(v_mat_list[[1]][clus_true_list[[id_clus]], ])
-  }
-  
-  
   
   # Generate expected number of spikes --------------------------------------------
   center_N_spks_mat = matrix(nrow=N_clus,ncol=2)
@@ -52,8 +47,8 @@ generate_data = function(SEED=NULL,
     center_N_spks_mat[2,1] = N_spks_total*0.3
     center_N_spks_mat[2,2] = N_spks_total*0.7
   } else if (N_clus==1){
-    center_N_spks_mat[1,1] = N_spks_total*(N_spks_ratio/(N_spks_ratio+1))
-    center_N_spks_mat[1,2] = N_spks_total*(1/(N_spks_ratio+1))
+    center_N_spks_mat[1,1] = N_spks_total*(1/(N_spks_ratio+1))
+    center_N_spks_mat[1,2] = N_spks_total*(N_spks_ratio/(N_spks_ratio+1))
   } else if (N_clus==4){
     center_N_spks_mat[1,1] = N_spks_total*0.5
     center_N_spks_mat[1,2] = N_spks_total*0.5
@@ -93,43 +88,15 @@ generate_data = function(SEED=NULL,
     center_density_array_true[2,2,] = center_density_array_true[2,2,]*center_N_spks_mat[2,2]/sum(center_N_spks_mat[2,1:2])
     
   } else if(N_clus==1){
-    s_tmp = u_0/10*sd_shrinkage; mu_tmp = -u_0+s_tmp; 
-    s_tmp_prime = u_0/3; mu_tmp_prime = -u_0/2+s_tmp_prime; 
-    center_density_array_true[1,1, ] = (2/3)*1/(2*s_tmp)*( 1 + cos(((t_vec_extend - mu_tmp)/s_tmp)*pi) ) * I(mu_tmp-s_tmp<=t_vec_extend & t_vec_extend<=mu_tmp+s_tmp) + 
-      (1/3)*1/(2*s_tmp_prime)*( 1 + cos(((t_vec_extend - mu_tmp_prime)/s_tmp_prime)*pi) ) * I(mu_tmp_prime-s_tmp_prime<=t_vec_extend & t_vec_extend<=mu_tmp_prime+s_tmp_prime)
+    s_tmp = 3/10; mu_tmp = -0.5; 
+    center_density_array_true[1,1, ] = 1/(2*s_tmp)*( 1 + cos(((t_vec - mu_tmp)/s_tmp)*pi) ) * I(mu_tmp-s_tmp<=t_vec & t_vec<=mu_tmp+s_tmp) 
     
-    s_tmp = sqrt(u_1)/5*sqrt(sd_shrinkage)
-    center_density_array_true[1,2, ] = 1/(2*s_tmp)^2*( 1 + cos(((sqrt(abs(t_vec_extend)) - s_tmp)/s_tmp) * pi) ) * I(0<=t_vec_extend & t_vec_extend<=(2*s_tmp)^2) 
-    
-    ### Add constant density to Component_1 before -u_0
-    center_density_array_true[1,1, ] = (1-c_1)*center_density_array_true[1,1, ] + 
-      c_1*c(rep(0,length(t_vec_extend[t_vec_extend < -u_0])-round(delta_1/t_unit)),
-            rep(1/delta_1,round(delta_1/t_unit)), 
-            rep(0,length(t_vec_extend[t_vec_extend >= -u_0])) )
-    ### Add constant density to Component_1 before u_1
-    center_density_array_true[1,1, ] = (1-c_2)*center_density_array_true[1,1, ] + 
-      c_2*c(rep(0,length(t_vec_extend)-round(delta_2/t_unit)),
-            rep(1/delta_2,round(delta_2/t_unit)) )
-    ### Add constant density to Component_2 before u_1
-    center_density_array_true[1,2, ] = (1-c_3)*center_density_array_true[1,2, ] + 
-      c_3*c(rep(0,length(t_vec_extend)-round(delta_3/t_unit)),
-            rep(1/delta_3,round(delta_3/t_unit)) )
-    
-    ### Force the two components to be identical up to a time shift
-    if(identical_components == TRUE){
-      center_density_array_true[1,1,] = c(rep(0, length(which(t_vec_extend < -u_0))),
-                                          center_density_array_true[1,2,][which(t_vec_extend>=0)],
-                                          rep(0, length(t_vec_extend) - 
-                                                length(which(t_vec_extend < -u_0)) - 
-                                                length(which(t_vec_extend >= 0)))
-      )
-    }
+    s_tmp = 1/5; mu_tmp = 0.5; 
+    center_density_array_true[1,2, ] = 1/(2*s_tmp)*( 1 + cos(((t_vec - mu_tmp)/s_tmp)*pi) ) * I(mu_tmp-s_tmp<=t_vec & t_vec<=mu_tmp+s_tmp) 
     
     ### Add weights for two components
     center_density_array_true[1,1,] = center_density_array_true[1,1,]*center_N_spks_mat[1,1]/sum(center_N_spks_mat[1,1:2])
     center_density_array_true[1,2,] = center_density_array_true[1,2,]*center_N_spks_mat[1,2]/sum(center_N_spks_mat[1,1:2])
-    
-    
     
   } else if (N_clus==4) {
     ## Clus 1
