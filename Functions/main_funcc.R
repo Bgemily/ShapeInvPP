a6985a32a9e44e99ee403c231b1910d5292b16d1
Zitong Zhang <zztzhang@ucdavis.edu>
2022-10-06 21:40:51 -0700

    SHAP-15 Change the model fitted by FunCC

--- Functions/main_funcc.R
+++ Functions/main_funcc.R
@@ -83,7 +83,7 @@ main_funcc = function(### Parameters for generative model
   ### Apply kCFC --------------------------------
   res = FunCC::funcc_biclust(fun_mat = density_array, 
                              delta = delta, theta = theta,
-                             alpha = 1, beta = 1, shift.alignement=TRUE, 
+                             alpha = 0, beta = 0, shift.alignement=TRUE, 
                              max.iter.align = 10)
   # Get estimated clusters
   N_biclus = res[[1]]@Number
