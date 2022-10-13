d60048f40bc64eb58141ec5f9a161b8dbfe1f4c6
Zitong Zhang <zztzhang@ucdavis.edu>
2022-10-12 21:40:16 -0700

    SHAP-33 Change generated synthetic data in order to make MISE_f monotonically decreases as cluster separability increases.

--- Functions/generate_data.R
+++ Functions/generate_data.R
@@ -102,31 +102,31 @@ generate_data = function(SEED=NULL,
     
   } else if (N_clus==4) {
     ## Clus 1
-    s_tmp = u_0*(1/2)*(1/(clus_sep^2)); mu_tmp = -u_0*(1/2); 
+    s_tmp = u_0*(1/8)*(1); mu_tmp = -u_0*(1/2); 
     center_density_array_true[1,1, ] = 1/(2*s_tmp)*( 1 + cos(((t_vec_extend - mu_tmp)/s_tmp)*pi) ) * I(mu_tmp-s_tmp<=t_vec_extend & t_vec_extend<=mu_tmp+s_tmp) 
     
-    s_tmp = sqrt(u_1)*(1/2/sqrt(2))*(1/clus_sep); mu_tmp = s_tmp
+    s_tmp = sqrt(u_1)*(1/4/sqrt(2))*(1); mu_tmp = s_tmp
     center_density_array_true[1,2, ] = 1/(2*s_tmp*2*mu_tmp)*( 1 + cos(((sqrt(abs(t_vec_extend)) - mu_tmp)/s_tmp)*pi) ) * I((mu_tmp-s_tmp)^2<=t_vec_extend & t_vec_extend<=(mu_tmp+s_tmp)^2) 
     
     ## Clus 2
-    s_tmp = u_0*(1/2)*(1/clus_sep); mu_tmp = -u_0*(1/2); 
+    s_tmp = u_0*(1/8)*(clus_sep); mu_tmp = -u_0*(1/2); 
     center_density_array_true[2,1, ] = 1/(2*s_tmp)*( 1 + cos(((t_vec_extend - mu_tmp)/s_tmp)*pi) ) * I(mu_tmp-s_tmp<=t_vec_extend & t_vec_extend<=mu_tmp+s_tmp) 
     
-    s_tmp = sqrt(u_1)*(1/2/sqrt(2))*(1/sqrt(clus_sep)); mu_tmp = s_tmp
+    s_tmp = sqrt(u_1)*(1/4/sqrt(2))*(sqrt(clus_sep)); mu_tmp = s_tmp
     center_density_array_true[2,2, ] = 1/(2*s_tmp*2*mu_tmp)*( 1 + cos(((sqrt(abs(t_vec_extend)) - mu_tmp)/s_tmp)*pi) ) * I((mu_tmp-s_tmp)^2<=t_vec_extend & t_vec_extend<=(mu_tmp+s_tmp)^2) 
 
     ## Clus 3
-    s_tmp = u_0*(1/2)*1; mu_tmp = -u_0*(1/2) 
+    s_tmp = u_0*(1/8)*(clus_sep^2); mu_tmp = -u_0*(1/2) 
     center_density_array_true[3,1, ] = 1/(2*s_tmp)*( 1 + cos(((t_vec_extend - mu_tmp)/s_tmp)*pi) ) * I(mu_tmp-s_tmp<=t_vec_extend & t_vec_extend<=mu_tmp+s_tmp) 
     
-    s_tmp = sqrt(u_1)*(1/2/sqrt(2))*1; mu_tmp = s_tmp
+    s_tmp = sqrt(u_1)*(1/4/sqrt(2))*(clus_sep); mu_tmp = s_tmp
     center_density_array_true[3,2, ] = 1/(2*s_tmp*2*mu_tmp)*( 1 + cos(((sqrt(abs(t_vec_extend)) - mu_tmp)/s_tmp)*pi) ) * I((mu_tmp-s_tmp)^2<=t_vec_extend & t_vec_extend<=(mu_tmp+s_tmp)^2) 
     
     ## Clus 4
-    s_tmp = u_0*(1/2)*(1/(clus_sep^2)); mu_tmp = -u_0*(1/2); 
+    s_tmp = u_0*(1/8)*(1); mu_tmp = -u_0*(1/2); 
     center_density_array_true[4,1, ] = 1/(2*s_tmp)*( 1 + cos(((t_vec_extend - mu_tmp)/s_tmp)*pi) ) * I(mu_tmp-s_tmp<=t_vec_extend & t_vec_extend<=mu_tmp+s_tmp) 
     
-    s_tmp = sqrt(u_1)*(1/2/sqrt(2))*1; mu_tmp = s_tmp
+    s_tmp = sqrt(u_1)*(1/4/sqrt(2))*(clus_sep); mu_tmp = s_tmp
     center_density_array_true[4,2, ] = 1/(2*s_tmp*2*mu_tmp)*( 1 + cos(((sqrt(abs(t_vec_extend)) - mu_tmp)/s_tmp)*pi) ) * I((mu_tmp-s_tmp)^2<=t_vec_extend & t_vec_extend<=(mu_tmp+s_tmp)^2) 
     
     ### Add weights (prop to N_spks) for two components
