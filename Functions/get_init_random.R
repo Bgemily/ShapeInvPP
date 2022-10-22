cb706cd6489bc0ad1b4e410c45a7c0be3161e02d
Zitong Zhang <zztzhang@ucdavis.edu>
2022-10-21 22:12:36 -0700

    SHAP-38 Initialize time shifts by earliest spike time; initialize memberships by applying k-means to (non-shifted) subject-wise densities.

--- Functions/get_init_random.R
+++ Functions/get_init_random.R
@@ -40,7 +40,7 @@ get_init_random = function(spks_time_mlist, stim_onset_vec,
       v_mat_list = rep(list(v_vec), N_component)
     }
   } else{
-    ### Use median of spike times as time shifts
+    ### Use earliest spike times as time shifts
     v_mat_list = rep(list(matrix(0, nrow = N_node, ncol = N_replicate)), N_component)
     for (id_node in 1:N_node) {
       for (id_replicate in 1:N_replicate) {
@@ -51,12 +51,16 @@ get_init_random = function(spks_time_mlist, stim_onset_vec,
           spks_time_curr_comp_vec = spks_time_tmp[which(spks_time_tmp >= time_start_curr_comp &
                                                           spks_time_tmp <= time_end_curr_comp)]
           if (length(spks_time_curr_comp_vec) > 0) {
-            v_mat_list[[id_component]][id_node, id_replicate] = quantile(spks_time_curr_comp_vec, 0.05) 
+            if (TRUE) {
+              v_mat_list[[id_component]][id_node, id_replicate] = runif(n = 1, min = min(t_vec), max = quantile(spks_time_curr_comp_vec, 0.05) )
+            } else {
+              v_mat_list[[id_component]][id_node, id_replicate] = quantile(spks_time_curr_comp_vec, 0.05) 
+            }
+            
           }
         }
       }
     }
-    
     ### Force minimum time shifts in each component to be zero
     for (id_component in 1:N_component){
       v_mat_list[[id_component]] = v_mat_list[[id_component]] - min(v_mat_list[[id_component]])
@@ -70,44 +74,61 @@ get_init_random = function(spks_time_mlist, stim_onset_vec,
   }
   
   
-  ### Force minimum time shifts in each component to be zero
-  for (id_component in 1:N_component){
-    v_mat_list[[id_component]] = v_mat_list[[id_component]] - min(v_mat_list[[id_component]])
-    v_mat_list[[id_component]] = round(v_mat_list[[id_component]]/t_unit)*t_unit
-  }
-  
-  
-  
-  # Initialize densities -------------
-  center_density_array = array(dim = c(N_clus, N_component, length(t_vec)) )
-  center_Nspks_mat = matrix(nrow = N_clus, ncol = N_component)
-  ind_random = sample(1:(N_node), size = N_clus, replace = FALSE)
-  for (id_clus in 1:N_clus) {
-    ind = ind_random[id_clus]
-    ind_2 = sample(1:N_replicate, size = 1)
-    spks_time_vec = spks_time_mlist[ind,ind_2][[1]]
-    tmp = get_smoothed_pp(event_time_vec = spks_time_vec, 
+  # Initialize clusters ---------------------------------------
+  ### Smooth the point process 
+  node_intensity_array = array(dim=c(N_node, 1, length(t_vec)))
+  node_density_array = array(dim=c(N_node, 1, length(t_vec)))
+  node_Nspks_mat = matrix(nrow=N_node, ncol=1)
+  for (id_node in 1:N_node) {
+    spks_time_vec_tmp = unlist(spks_time_mlist[id_node, ])
+    tmp = get_smoothed_pp(event_time_vec = spks_time_vec_tmp, 
                           freq_trun = freq_trun, 
                           t_vec = t_vec, 
                           bw=bw)
-    intensity = tmp$intens_vec
-    density = intensity/length(spks_time_vec)
-    for (id_component in 1:N_component) {
-      density_curr_component = I((t_vec >= key_times_vec[id_component]) &
-                                   (t_vec <= key_times_vec[id_component+1])) * density
-      center_density_array[id_clus, id_component, ] = density_curr_component
-      center_Nspks_mat[id_clus, id_component] = length(spks_time_vec) * sum(density_curr_component * t_unit)
+    intensity_tmp = tmp$intens_vec
+    N_spks_q = length(spks_time_vec_tmp)
+    if (N_spks_q>0) {
+      density_tmp = intensity_tmp / N_spks_q
+    } else{
+      density_tmp = intensity_tmp*0
     }
+    N_spks_nodetrial_vec_tmp = sapply(spks_time_mlist[id_node, , drop=FALSE], length)
+    F_hat_tmp = sqrt( mean(N_spks_nodetrial_vec_tmp^2) )
+    intensity_tmp = density_tmp*F_hat_tmp
     
+    node_intensity_array[id_node,1,] = intensity_tmp
+    node_density_array[id_node,1,] = density_tmp
+    node_Nspks_mat[id_node,1] = F_hat_tmp
+  }
+  
+  ### Apply k-means algorithm
+  if (rmv_conn_prob){
+    membership = kmeans(x=node_density_array[,1,], centers = N_clus, nstart = N_start_kmean)$cluster
+  } else{
+    membership = kmeans(x=node_intensity_array[,1,], centers = N_clus, nstart = N_start_kmean)$cluster
+  }
+  clusters = mem2clus(membership = membership, N_clus_min = N_clus)
+  clusters_list = clusters
+  membership_vec = membership
+  
+  ### Force minimum time shifts in each (cluster, component) to be zero
+  if (!fix_timeshift) {
+    for (id_clus in 1:N_clus) {
+      for (id_component in 1:N_component){
+        v_mat_list[[id_component]][clusters_list[[id_clus]], ] = v_mat_list[[id_component]][clusters_list[[id_clus]], ] - (quantile(v_mat_list[[id_component]][clusters_list[[id_clus]], ], 0.0)-0)
+        v_mat_list[[id_component]] = round(v_mat_list[[id_component]]/t_unit)*t_unit
+      }
+    }
+    ### Force time shifts of first component to be truth
+    if( (!fix_timeshift) & fix_comp1_timeshift_only ){
+      v_mat_list[[1]] = v_true_mat_list[[1]]
+    }
   }
   
   
   return(list(v_vec=NA,
               v_mat_list=v_mat_list,
-              membership_vec=NA, 
-              clusters_list=NA,
-              center_density_array = center_density_array,
-              center_Nspks_mat = center_Nspks_mat,
-              center_intensity_array = NA))
+              membership_vec=membership_vec, 
+              clusters_list=clusters_list ))
 }
 
