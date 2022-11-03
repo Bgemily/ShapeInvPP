942ddfb0132f953bfc333e057042f70fc6388553
Zitong Zhang <zztzhang@ucdavis.edu>
2022-11-03 16:51:06 -0700

    SHAP-55 Rename variables

--- Functions/get_indiv_intensity_array.R
+++ Functions/get_indiv_intensity_array.R
@@ -8,9 +8,9 @@ get_indiv_intensity_array = function(spks_time_mlist,
   N_trial = ncol(spks_time_mlist)
   
   
-  subj_density_array = array(dim = c(N_subj, N_trial, length(t_vec)))
-  subj_intensity_array = array(dim = c(N_subj, N_trial, length(t_vec)))
-  fft_subj_density_array = array(dim = c(N_subj, N_trial, length(t_vec)))
+  subjtrial_density_array = array(dim = c(N_subj, N_trial, length(t_vec)))
+  subjtrial_intensity_array = array(dim = c(N_subj, N_trial, length(t_vec)))
+  fft_subjtrial_density_array = array(dim = c(N_subj, N_trial, length(t_vec)))
   N_spks_mat = matrix(0, nrow = N_subj, ncol = N_trial)
   for (id_subj in 1:N_subj) {
     for (id_trial in 1:N_trial) {
@@ -20,23 +20,23 @@ get_indiv_intensity_array = function(spks_time_mlist,
                             freq_trun = freq_trun, 
                             t_vec = t_vec, 
                             bw = bw)
-      subj_intensity = tmp$intens_vec
-      subj_intensity_array[id_subj, id_trial, ] = subj_intensity
+      subjtrial_intensity = tmp$intens_vec
+      subjtrial_intensity_array[id_subj, id_trial, ] = subjtrial_intensity
       
-      subj_density = subj_intensity / (length(spks_time_vec)+.Machine$double.eps)
-      subj_density_array[id_subj, id_trial, ] = subj_density
+      subjtrial_density = subjtrial_intensity / (length(spks_time_vec)+.Machine$double.eps)
+      subjtrial_density_array[id_subj, id_trial, ] = subjtrial_density
       
-      fft_subj_density_tmp = fft(subj_density) / length(subj_density)
-      fft_subj_density_array[id_subj, id_trial, ] = fft_subj_density_tmp
+      fft_subjtrial_density_tmp = fft(subjtrial_density) / length(subjtrial_density)
+      fft_subjtrial_density_array[id_subj, id_trial, ] = fft_subjtrial_density_tmp
       
       N_spks_mat[id_subj, id_trial] = length(spks_time_vec)
     }
   }
   
   
-  return(list(subj_density_array = subj_density_array,
-              subj_intensity_array = subj_intensity_array,
-              fft_subj_density_array = fft_subj_density_array,
+  return(list(subjtrial_density_array = subjtrial_density_array,
+              subjtrial_intensity_array = subjtrial_intensity_array,
+              fft_subjtrial_density_array = fft_subjtrial_density_array,
               N_spks_mat = N_spks_mat ))
 }
 
