651c8c41223c3e8729894ec3fddd8e7d9d62096c
Zitong Zhang <zztzhang@ucdavis.edu>
2022-11-04 21:49:07 -0700

    SHAP-55 Attempt to improve initialization

--- Functions/get_init.R
+++ Functions/get_init.R
@@ -50,13 +50,17 @@ get_init = function(spks_time_mlist,
         spks_time_shifted_vec = c()
         for (id_trial in 1:N_trial) {
           spks_time_tmp = spks_time_mlist[id_subj, id_trial][[1]]
-          time_start_curr_comp = key_times_vec[id_component] 
-          time_end_curr_comp = key_times_vec[id_component + 1]
+          time_start_curr_comp = key_times_vec[id_component] + v_trialwise_vec_list[[id_component]][id_trial]
+          if ((id_component + 1) <= N_component) {
+            time_end_curr_comp = key_times_vec[id_component + 1] + v_trialwise_vec_list[[id_component + 1]][id_trial]
+          } else {
+            time_end_curr_comp = max(t_vec)
+          }
+          
           spks_time_curr_comp_vec = spks_time_tmp[which(spks_time_tmp >= time_start_curr_comp &
                                                           spks_time_tmp <= time_end_curr_comp)]
           if (length(spks_time_curr_comp_vec) > 0) {
             spks_time_curr_comp_vec = spks_time_curr_comp_vec - v_trialwise_vec_list[[id_component]][id_trial]
-            # v_mat_list[[id_component]][id_subj, id_trial] = quantile(spks_time_curr_comp_vec, 0.05) 
           }
           spks_time_shifted_vec = c(spks_time_shifted_vec, spks_time_curr_comp_vec)
         }
