b980cef21f0db7f778b9374798ecec53ab664fe3
Zitong Zhang <zztzhang@ucdavis.edu>
2023-04-12 02:17:26 -0700

    SHAP-71 Change setup for initialization comparison

--- main_run_sim_init_compr.R
+++ main_run_sim_init_compr.R
@@ -35,100 +35,57 @@ test_algorithm_restart = FALSE
 save_res_details = TRUE
 
 top_level_folder = "../Results/Rdata"
-setup = 'Multi_restart_v3.1.4'
+setup = 'Init_compr_v3.1.4'
+default_setting = 'N_spks_total=70,N_subj=100,N_clus=4,clus_sep=1.4,key_time_comp2=-0.2'
 
-if (test_random_restart) {
-  method = paste0('shape_inv_pp_', 'Nrestart_algo1_kmean5' )
-  default_setting = 'N_spks_total=70,N_subj=200,N_clus=4,clus_sep=1.4,N_comp=2'
+### Parameters' possible values:
+timeshift_subj_max_vec_list = list(c(1/32/4, 1/32)*2, c(1/32/4, 1/32)*4, 
+                                   c(1/32/4, 1/32)*6, c(1/32/4, 1/32)*8,
+                                   c(1/32/4, 1/32)*10, c(1/32/4, 1/32)*12)
+N_subj_list = list(100, 120, 140, 160, 180, 200)
+key_times_vec_list = list(c(-1,0-0.2,1.5), c(-1,0.05-0.2,1.5), c(-1,0.1-0.2,1.5), 
+                          c(-1,0.15-0.2,1.5), c(-1,0.2-0.2,1.5), c(-1,0.25-0.2,1.5))
+
+
+N_restart_algo_list = list(1, 3, 10)
+for (id_method in 1:length(N_restart_algo_list)){
+  N_restart = N_restart_algo_list[[id_method]]
+  method = paste0('shape_inv_pp_', 'Rand_init_v2_Nrestart_algo', as.character(N_restart) )
   for (id_N_split in 1:N_split) {
     if (save_res_details & (id_N_split == 1)) {
       save_center_pdf_array = TRUE
     } else {
       save_center_pdf_array = FALSE
     }
-    results <- foreach(id_replicate = 1:N_replicate) %dopar% {
-      SEED = id_N_split * 1000 + id_replicate + 10
-      print(paste0("SEED: ", SEED))
-      tryCatch(main_shapeinvpp(SEED = SEED,
-                               N_subj = 200,
-                               N_clus = 4,
-                               N_component_true = 2,
-                               t_vec = seq(-1,1.5,0.01),
-                               timeshift_subj_max_vec = c(1/32/4, 1/32)*12,
-                               N_spks_total = 70,
-                               N_trial = 1,
-                               clus_sep = 1.4,
-                               ### Parameters for algorithms
-                               N_restart = 1, 
-                               N_start_kmean = 5,
-                               MaxIter = 10,
-                               conv_thres = -Inf, 
-                               freq_trun = 10, 
-                               gamma = 1,
-                               N_component = 2,
-                               key_times_vec = c(-1,0-0.2,1.5),
-                               fix_timeshift = FALSE,
-                               fix_membership = FALSE,
-                               save_center_pdf_array = save_center_pdf_array ),
-               error = function(e) print(paste0("SEED = ", SEED, " : ", e)) )
-    }
-    param_name = "N_trial"
-    param_value = 1
-    folder_path = paste0(top_level_folder, '/', setup,
-                         '/', method, 
-                         '/', default_setting,
-                         '/', param_name, '/', param_value)
-    dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
-    
-    now_replicate = format(Sys.time(), "%Y%m%d_%H%M%S")
-    save(results, file = paste0(folder_path, '/', 'N_replicate', N_replicate, '_', now_replicate, '.Rdata'))
-    rm(results)
-    
-  }
-  
-  N_restart_algo_list = list(1, 3, 10)
-  for (id_method in 1:length(N_restart_algo_list)) {
-    N_restart = N_restart_algo_list[[id_method]]
-    method = paste0('shape_inv_pp_', 'Rand_init_v2_Nrestart_algo', as.character(N_restart) )
-    default_setting = 'N_spks_total=70,N_subj=100,N_clus=4,clus_sep=1.4,N_comp=2'
-    for (id_N_split in 1:N_split) {
-      if (save_res_details & (id_N_split == 1)) {
-        save_center_pdf_array = TRUE
-      } else {
-        save_center_pdf_array = FALSE
-      }
-      
-      results <- foreach(id_replicate = 1:N_replicate) %dopar% {
-        SEED = id_N_split * 1000 + id_replicate + 10
-        print(paste0("SEED: ", SEED))
-        tryCatch(main_shapeinvpp(SEED = SEED,
-                                 N_subj = 200,
-                                 N_clus = 4,
+    for (id_timeshift_subj_max_vec in 1:length(timeshift_subj_max_vec_list)) {
+      timeshift_subj_max_vec = timeshift_subj_max_vec_list[[id_timeshift_subj_max_vec]]
+      results <- foreach(j = 1:N_replicate) %dopar% {
+        SEED = sample(1:1e7,1)
+        tryCatch(main_shapeinvpp(SEED = SEED, 
+                                 N_subj = N_subj_list[[1]],
+                                 N_clus = 4, 
                                  N_component_true = 2,
-                                 t_vec = seq(-1,1.5,0.01),
-                                 timeshift_subj_max_vec = c(1/32/4, 1/32)*12,
-                                 ### params when N_clus==4:
                                  N_spks_total = 70,
-                                 N_trial = 1,
+                                 timeshift_subj_max_vec = timeshift_subj_max_vec,
+                                 t_vec = seq(-1,1.5,0.01),
                                  clus_sep = 1.4,
                                  ### Parameters for algorithms
                                  rand_init = TRUE,
-                                 N_restart = N_restart, 
-                                 N_start_kmean = 1, 
-                                 MaxIter = 10,
-                                 conv_thres = -Inf, 
-                                 freq_trun = 10, 
+                                 N_restart = N_restart,
+                                 N_start_kmean = 1,
+                                 freq_trun = 10,
                                  gamma = 1,
                                  N_component = 2,
-                                 key_times_vec = c(-1,0-0.2,1.5),
+                                 key_times_vec = key_times_vec_list[[1]],
                                  fix_timeshift = FALSE,
                                  fix_membership = FALSE,
-                                 save_center_pdf_array = save_center_pdf_array ),
+                                 save_center_pdf_array = save_center_pdf_array),
                  error = function(e) print(paste0("SEED = ", SEED, " : ", e)) )
       }
-      param_name = "N_trial"
-      param_value = 1
-      folder_path = paste0(top_level_folder, '/', setup,
+      param_name = "timeshift_subj_max_vec"
+      param_value = paste0(timeshift_subj_max_vec, collapse = '_')
+      folder_path = paste0(top_level_folder,
+                           '/', setup,
                            '/', method, 
                            '/', default_setting,
                            '/', param_name, '/', param_value)
@@ -137,74 +94,87 @@ if (test_random_restart) {
       now_replicate = format(Sys.time(), "%Y%m%d_%H%M%S")
       save(results, file = paste0(folder_path, '/', 'N_replicate', N_replicate, '_', now_replicate, '.Rdata'))
       rm(results)
+    }
+    for (id_key_times_vec in 1:length(key_times_vec_list)) {
+      key_times_vec = key_times_vec_list[[id_key_times_vec]]
+      results <- foreach(j = 1:N_replicate) %dopar% {
+        SEED = sample(1:1e7,1)
+        tryCatch(main_shapeinvpp(SEED = SEED, 
+                                 N_subj = N_subj_list[[1]],
+                                 N_clus = 4, 
+                                 N_component_true = 2,
+                                 N_spks_total = 70,
+                                 timeshift_subj_max_vec = timeshift_subj_max_vec_list[[1]],
+                                 t_vec = seq(-1,1.5,0.01),
+                                 ### params when N_clus==4:
+                                 clus_sep = 1.4,
+                                 ### Parameters for algorithms
+                                 rand_init = TRUE,
+                                 N_restart = N_restart,
+                                 N_start_kmean = 1,
+                                 freq_trun = 10,
+                                 gamma = 1,
+                                 N_component = 2,
+                                 key_times_vec = key_times_vec,
+                                 fix_timeshift = FALSE,
+                                 fix_membership = FALSE,
+                                 save_center_pdf_array = save_center_pdf_array),
+                 error = function(e) print(paste0("SEED = ", SEED, " : ", e)) )
+      }
+      param_name = "key_times_vec"
+      param_value = paste0(key_times_vec, collapse = '_')
+      folder_path = paste0(top_level_folder,
+                           '/', setup,
+                           '/', method, 
+                           '/', default_setting,
+                           '/', param_name, '/', param_value)
+      dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
       
+      now_replicate = format(Sys.time(), "%Y%m%d_%H%M%S")
+      save(results, file = paste0(folder_path, '/', 'N_replicate', N_replicate, '_', now_replicate, '.Rdata'))
+      rm(results)
+    }
+    for (id_N_subj in 1:length(N_subj_list)) {
+      N_subj = N_subj_list[[id_N_subj]]
+      results <- foreach(j = 1:N_replicate) %dopar% {
+        SEED = sample(1:1e7,1)
+        tryCatch(main_shapeinvpp(SEED = SEED, 
+                                 N_subj = N_subj,
+                                 N_clus = 4, 
+                                 N_component_true = 2,
+                                 N_spks_total = 70,
+                                 timeshift_subj_max_vec = timeshift_subj_max_vec_list[[1]],
+                                 t_vec = seq(-1,1.5,0.01),
+                                 ### params when N_clus==4:
+                                 clus_sep = 1.4,
+                                 ### Parameters for algorithms
+                                 rand_init = TRUE,
+                                 N_restart = N_restart,
+                                 N_start_kmean = 1,
+                                 freq_trun = 10,
+                                 gamma = 1,
+                                 N_component = 2,
+                                 key_times_vec = key_times_vec_list[[1]],
+                                 fix_timeshift = FALSE,
+                                 fix_membership = FALSE,
+                                 save_center_pdf_array = save_center_pdf_array),
+                 error = function(e) print(paste0("SEED = ", SEED, " : ", e)) )
+      }
+      param_name = "N_subj"
+      param_value = N_subj
+      folder_path = paste0(top_level_folder,
+                           '/', setup,
+                           '/', method, 
+                           '/', default_setting,
+                           '/', param_name, '/', param_value)
+      dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
       
+      now_replicate = format(Sys.time(), "%Y%m%d_%H%M%S")
+      save(results, file = paste0(folder_path, '/', 'N_replicate', N_replicate, '_', now_replicate, '.Rdata'))
+      rm(results)
     }
   }
   
-  
 }
-
-
-if (test_algorithm_restart) {
-  ### Parameters' possible values:
-  N_trial_list = list(1,2,3,4,5)
-  N_restart_algo_N_start_kmean_list = list(c(1,5), c(1,1), c(3,1), c(5,1), c(3,5), c(10,1) )
-  for (id_method in 1:length(N_restart_algo_N_start_kmean_list)) {
-    N_restart = N_restart_algo_N_start_kmean_list[[id_method]][1]
-    N_start_kmean = N_restart_algo_N_start_kmean_list[[id_method]][2]
-    method = paste0('Nrestart', '_algo', as.character(N_restart), 
-                    '_kmean', as.character(N_start_kmean) )
-    default_setting = 'N_spks_total=100,N_subj=100,N_clus=4,clus_sep=1.3,N_comp=2'
-    for (id_N_split in 1:N_split) {
-      if (save_res_details & (id_N_split == 1)) {
-        save_center_pdf_array = TRUE
-      } else {
-        save_center_pdf_array = FALSE
-      }
-      for (id_N_trial in 1:length(N_trial_list)) {
-        N_trial = N_trial_list[[id_N_trial]]
-        results <- foreach(j = 1:N_replicate) %dopar% {
-          SEED = sample(1:1e7,1)
-          tryCatch(main_shapeinvpp(SEED = SEED,
-                               N_subj = 100,
-                               N_clus = 4,
-                               N_component_true = 2,
-                               t_vec = seq(-1, 1, by=0.01),
-                               timeshift_subj_max_vec = c(1/4, 1/16),
-                               ### params when N_clus==4:
-                               N_spks_total = 100,
-                               N_trial = N_trial,
-                               clus_sep = 1.3,
-                               ### Parameters for algorithms
-                               N_restart = N_restart, 
-                               N_start_kmean = N_start_kmean,
-                               freq_trun = 10, 
-                               N_component = 2,
-                               key_times_vec = c(-1,0,1),
-                               fix_timeshift = FALSE,
-                               fix_membership = FALSE,
-                               save_center_pdf_array = save_center_pdf_array ),
-                   error = function(e) print(paste0("SEED = ", SEED, " : ", e)) )
-        }
-        param_name = "N_trial"
-        param_value = N_trial
-        folder_path = paste0(top_level_folder, '/', setup,
-                             '/', method, 
-                             '/', default_setting,
-                             '/', param_name, '/', param_value)
-        dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
-        
-        now_replicate = format(Sys.time(), "%Y%m%d_%H%M%S")
-        save(results, file = paste0(folder_path, '/', 'N_replicate', N_replicate, '_', now_replicate, '.Rdata'))
-        rm(results)
-      }
-    }
-  }
-}
-
-
-
-
 
 
