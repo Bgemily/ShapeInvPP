9fd0b13a70929593c1a0c15b1d87fd472ea3d2de
Zitong Zhang <zztzhang@ucdavis.edu>
2023-09-05 00:45:09 -0700

    SHAP-76 Add more replicates

--- main_run_sim_compare_kCFC.R
+++ main_run_sim_compare_kCFC.R
@@ -19,8 +19,8 @@ library(parallel)
 
 # User input setup --------------------------------------------------------
 
-N_replicate_total = 20
-N_split = 2
+N_replicate_total = 200
+N_split = 20
 
 N_replicate = N_replicate_total/N_split
 
