15cfd34f430c06f37845f1203e7bf9354ef4e481
Zitong Zhang <zztzhang@ucdavis.edu>
2023-10-03 21:17:08 -0700

    Increase max_iter for the kmeans_align method

--- main_run_sim_compare_kmeans_align.R
+++ main_run_sim_compare_kmeans_align.R
@@ -37,7 +37,7 @@ save_res_details = TRUE
 
 top_level_folder = "../Results/Rdata"
 setup = 'Compare_methods_v3.1.8'
-method = 'kmeans_align_v2'
+method = 'kmeans_align_v3_maxiter50'
 
 ### Parameters' possible values:
 timeshift_subj_max_vec_list = list(c(1/32/4, 1/32)*2, c(1/32/4, 1/32)*3, 
