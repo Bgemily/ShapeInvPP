1963b1fbcf2af8a607f56e4f1aa30f779b2cdf0f
Zitong Zhang <zztzhang@ucdavis.edu>
2023-11-18 21:29:21 -0800

    SHAP-90 Test FPCA performance under new simulation setup.

--- main_run_sim_compare_FPCA.R
+++ main_run_sim_compare_FPCA.R
@@ -36,19 +36,18 @@ test_N_component_2 = TRUE
 save_res_details = TRUE
 
 top_level_folder = "../Results/Rdata"
-setup = 'Compare_methods_Nclus1_v1'
+setup = 'Compare_methods_Nclus1_v2'
 method = 'fpca'
 
 ### Parameters' possible values:
 timeshift_subj_max_vec_list = list(c(1/32/4, 1/32)*2, c(1/32/4, 1/32)*3, 
                                    c(1/32/4, 1/32)*4, c(1/32/4, 1/32)*5,
                                    c(1/32/4, 1/32)*6, c(1/32/4, 1/32)*7)
-N_subj_list = list(100, 120, 140, 160, 180, 200)
 key_times_vec_list = list(c(-1,0-0.2,1.5), c(-1,0.02-0.2,1.5), c(-1,0.04-0.2,1.5), 
                           c(-1,0.06-0.2,1.5), c(-1,0.08-0.2,1.5), c(-1,0.1-0.2,1.5))
 
 if (test_N_component_2){
-  default_setting = 'N_spks_total=70,N_subj=100,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2'
+  default_setting = 'N_spks_total=70,N_subj=25,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2'
   for (id_N_split in 1:N_split) {
     if (save_res_details & (id_N_split == 1)) {
       save_center_pdf_array = TRUE
@@ -61,7 +60,7 @@ if (test_N_component_2){
       results <- foreach(j = 1:N_replicate) %dopar% {
         SEED = sample(1:1e7,1)
         tryCatch(main_fpca(SEED = SEED,
-                           N_subj = N_subj_list[[1]],
+                           N_subj = 25,
                            N_clus = 1,
                            N_component_true = 2,
                            N_spks_total = 70,
@@ -92,7 +91,7 @@ if (test_N_component_2){
       results <- foreach(j = 1:N_replicate) %dopar% {
         SEED = sample(1:1e7,1)
         tryCatch(main_fpca(SEED = SEED, 
-                           N_subj = N_subj_list[[1]],
+                           N_subj = 25,
                            N_clus = 1, 
                            N_component_true = 2,
                            N_spks_total = 70,
@@ -108,38 +107,6 @@ if (test_N_component_2){
       }
       param_name = "key_times_vec"
       param_value = paste0(key_times_vec, collapse = '_')
-      folder_path = paste0(top_level_folder,
-                           '/', setup,
-                           '/', method, 
-                           '/', default_setting,
-                           '/', param_name, '/', param_value)
-      dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
-      
-      now_replicate = format(Sys.time(), "%Y%m%d_%H%M%S")
-      save(results, file = paste0(folder_path, '/', 'N_replicate', N_replicate, '_', now_replicate, '.Rdata'))
-      rm(results)
-    }
-    for (id_N_subj in 1:length(N_subj_list)) {
-      N_subj = N_subj_list[[id_N_subj]]
-      results <- foreach(j = 1:N_replicate) %dopar% {
-        SEED = sample(1:1e7,1)
-        tryCatch(main_fpca(SEED = SEED, 
-                           N_subj = N_subj,
-                           N_clus = 1, 
-                           N_component_true = 2,
-                           N_spks_total = 70,
-                           timeshift_subj_max_vec = timeshift_subj_max_vec_list[[1]],
-                           t_vec = seq(-1,1.5,0.01),
-                           ### params when N_clus==4:
-                           clus_sep = 1.4,
-                           ### Parameters for algorithms
-                           bw = 'SJ',
-                           N_component = 2,
-                           save_center_pdf_array = save_center_pdf_array),
-                 error = function(e) print(paste0("SEED = ", SEED, " : ", e)) )
-      }
-      param_name = "N_subj"
-      param_value = N_subj
       folder_path = paste0(top_level_folder,
                            '/', setup,
                            '/', method, 
