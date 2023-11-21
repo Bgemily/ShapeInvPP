6fb35b0149351ee4de4fd2ffd16313d51aec24b8
Zitong Zhang <zztzhang@ucdavis.edu>
2023-11-20 23:41:24 -0800

    SHAP-90 Add larger candidate values of N_trial, N_subj. Add new setting where N_subj varies, time shifts are unknown to the algorithm, and N_trial = 2.

--- main_run_sim_compare_ShapeInvPP_Nclus1.R
+++ main_run_sim_compare_ShapeInvPP_Nclus1.R
@@ -33,14 +33,14 @@ doParallel::registerDoParallel(cores = N_cores)
 save_res_details = TRUE
 
 top_level_folder = "../Results/Rdata"
-setup = 'Compare_methods_Nclus1_v2.4'
+setup = 'Compare_methods_Nclus1_v2.4.1'
 method = 'shape_inv_pp'
 
 ### Parameters' possible values:
 timeshift_trial_max_list = list(0, 0.1, 0.2)
-N_trial_list = list(1,2,3,4,5,6)
+N_trial_list = list(1,2,3,4,5,6,7,8,9,10)
 timeshift_subj_max_vec_list = list(c(1/32/4, 1/32)*2 )
-N_subj_list = list(25, 40, 55, 70, 85, 100)
+N_subj_list = list(25, 40, 55, 70, 85, 100, 115, 130, 145, 160)
 key_times_vec_list = list(c(-1,0-0.2,1.5), c(-1,0.04-0.2,1.5), c(-1,0.08-0.2,1.5),
                           c(-1,0.12-0.2,1.5), c(-1,0.16-0.2,1.5), c(-1,0.2-0.2,1.5))
 
@@ -144,8 +144,56 @@ for (use_true_timeshift in c(FALSE, TRUE)){
     }
   }
 }
+if (TRUE) {
+  default_setting = paste0('N_trial=2,N_spks_total=50,N_subj=25,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2')
+  for (id_N_split in 1:N_split) {
+    if (save_res_details & (id_N_split == 1)) {
+      save_center_pdf_array = TRUE
+    } else {
+      save_center_pdf_array = FALSE
+    }
+    for (id_N_subj in 1:length(N_subj_list)) {
+      N_subj = N_subj_list[[id_N_subj]]
+      results <- foreach(j = 1:N_replicate) %dopar% {
+        SEED = sample(1:1e7,1)
+        tryCatch(main_shapeinvpp(SEED = SEED, 
+                                 N_trial = 2,
+                                 N_subj = N_subj,
+                                 N_clus = 1, 
+                                 N_component_true = 2,
+                                 N_spks_total = 50,
+                                 timeshift_subj_max_vec = timeshift_subj_max_vec_list[[1]],
+                                 timeshift_trial_max = 0.2,
+                                 t_vec = seq(-1,1.5,0.01),
+                                 ### params when N_clus==4:
+                                 clus_sep = 1.4,
+                                 ### Parameters for algorithms
+                                 freq_trun = 10,
+                                 gamma = 1,
+                                 N_component = 2,
+                                 key_times_vec = key_times_vec_list[[1]],
+                                 fix_timeshift = FALSE,
+                                 fix_membership = FALSE,
+                                 save_center_pdf_array = save_center_pdf_array),
+                 error = function(e) print(paste0("SEED = ", SEED, " : ", e)) )
+      }
+      param_name = "N_subj"
+      param_value = N_subj
+      folder_path = paste0(top_level_folder,
+                           '/', setup,
+                           '/', method, 
+                           '/', default_setting,
+                           '/', param_name, '/', param_value)
+      dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
+      
+      now_replicate = format(Sys.time(), "%Y%m%d_%H%M%S")
+      save(results, file = paste0(folder_path, '/', 'N_replicate', N_replicate, '_', now_replicate, '.Rdata'))
+      rm(results)
+    }
+  }
+}
 
-if(TRUE){
+if(FALSE){
   timeshift_subj_max_vec_list = list(c(1/32/4, 1/32)*2, c(1/32/4, 1/32)*3,
                                      c(1/32/4, 1/32)*4, c(1/32/4, 1/32)*5,
                                      c(1/32/4, 1/32)*6, c(1/32/4, 1/32)*7 )
