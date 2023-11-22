0ea2286a3f2bfbf6a303eabb9beb4ead5f60e458
Zitong Zhang <zztzhang@ucdavis.edu>
2023-11-22 02:40:07 -0800

    SHAP-90 Increase duration between components

--- main_run_sim_compare_ShapeInvPP_Nclus1.R
+++ main_run_sim_compare_ShapeInvPP_Nclus1.R
@@ -200,7 +200,7 @@ if(TRUE){
                                      c(1/32/4, 1/32)*4, c(1/32/4, 1/32)*5,
                                      c(1/32/4, 1/32)*6, c(1/32/4, 1/32)*7 )
   
-  default_setting = 'N_trial=2,timeshift_trial_max=0.2,N_spks_total=50,N_subj=25,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2'
+  default_setting = 'N_trial=2,timeshift_trial_max=0.2,N_spks_total=50,N_subj=25,N_clus=1,clus_sep=1.4,key_time_comp2=0'
   for (id_N_split in 1:N_split) {
     if (save_res_details & (id_N_split == 1)) {
       save_center_pdf_array = TRUE
@@ -225,7 +225,7 @@ if(TRUE){
                                  freq_trun = 10,
                                  gamma = 1,
                                  N_component = 2,
-                                 key_times_vec = c(-1,0-0.2,1.5),
+                                 key_times_vec = c(-1,0,1.5),
                                  fix_timeshift = FALSE,
                                  fix_membership = FALSE,
                                  save_center_pdf_array = save_center_pdf_array),
