39c0340ef3ada3bbbe403c4dfdce7593cd0ce358
Zitong Zhang <zztzhang@ucdavis.edu>
2023-12-05 10:51:10 -0800

    SHAP-91 Not save result details

--- main_run_sim_compare_ShapeInvPP.R
+++ main_run_sim_compare_ShapeInvPP.R
@@ -31,7 +31,7 @@ doParallel::registerDoParallel(cores = N_cores)
 
 # Run simulations ---------------------------------------------------------
 test_N_component_2 = TRUE
-save_res_details = TRUE
+save_res_details = FALSE
 
 top_level_folder = "../Results/Rdata"
 setup = 'Compare_methods_Nclus4_v4'
