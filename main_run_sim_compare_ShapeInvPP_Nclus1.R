cab3d6eeef1ec9535f8dfe50f87b2559f905d007
Zitong Zhang <zztzhang@ucdavis.edu>
2023-12-14 14:13:26 -0800

    SHAP-91 Test performance of shapeinvpp on synthetic data with N_clus=1 and non-zero baseline intensity.

--- main_run_sim_compare_ShapeInvPP_Nclus1.R
+++ main_run_sim_compare_ShapeInvPP_Nclus1.R
@@ -18,8 +18,8 @@ library(parallel)
 
 # User input setup --------------------------------------------------------
 
-N_replicate_total = 200
-N_split = 20
+N_replicate_total = 20
+N_split = 2
 
 N_replicate = N_replicate_total/N_split
 
@@ -33,20 +33,20 @@ doParallel::registerDoParallel(cores = N_cores)
 save_res_details = FALSE
 
 top_level_folder = "../Results/Rdata"
-setup = 'Compare_methods_Nclus1_v3'
+setup = 'Compare_methods_Nclus1_v3.1'
 method = 'shape_inv_pp'
 
 ### Parameters' possible values:
 timeshift_trial_max_list = list(0, 0.05, 0.1, 0.2)
 N_trial_list = list(1,2,3,4,5,6,7,8,9,10)
 timeshift_subj_max_vec_list = list(c(1/32/4, 1/32)*2 )
-N_subj_list = list(25, 50, 75, 100, 125, 150, 175, 200, 225, 250)
+N_subj_list = list(10,20,30,40,50,60,70,80,90,100)
 key_times_vec_list = list(c(-1,0-0.2,1.5), c(-1,0.04-0.2,1.5), c(-1,0.08-0.2,1.5),
                           c(-1,0.12-0.2,1.5), c(-1,0.16-0.2,1.5), c(-1,0.2-0.2,1.5))
 if (TRUE) {
   for (timeshift_trial_max in timeshift_trial_max_list){
     default_setting = paste0("timeshift_trial_max=",timeshift_trial_max,",", 
-                             'N_spks_total=50,N_subj=25,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2')
+                             'N_spks_total=50,N_subj=10,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2')
     for (id_N_split in 1:N_split) {
       if (save_res_details & (id_N_split == 1)) {
         save_center_pdf_array = TRUE
@@ -96,7 +96,7 @@ if (TRUE) {
   
   for (use_true_timeshift in c(FALSE, TRUE)){
     default_setting = paste0("use_true_timeshift_", use_true_timeshift,",", 
-                             'N_spks_total=50,N_subj=25,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2')
+                             'N_spks_total=50,N_subj=10,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2')
     for (id_N_split in 1:N_split) {
       if (save_res_details & (id_N_split == 1)) {
         save_center_pdf_array = TRUE
@@ -145,7 +145,7 @@ if (TRUE) {
     }
   }
   if (TRUE) {
-    default_setting = paste0('N_trial=2,N_spks_total=50,N_subj=25,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2')
+    default_setting = paste0('N_trial=2,N_spks_total=50,N_subj=10,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2')
     for (id_N_split in 1:N_split) {
       if (save_res_details & (id_N_split == 1)) {
         save_center_pdf_array = TRUE
@@ -201,7 +201,7 @@ if(TRUE){
                                      c(1/32/4, 1/32)*4, c(1/32/4, 1/32)*5,
                                      c(1/32/4, 1/32)*6, c(1/32/4, 1/32)*7 )
   
-  default_setting = 'N_trial=2,N_spks_total=50,N_subj=25,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2'
+  default_setting = 'N_trial=2,N_spks_total=50,N_subj=10,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2'
   for (id_N_split in 1:N_split) {
     if (save_res_details & (id_N_split == 1)) {
       save_center_pdf_array = TRUE
