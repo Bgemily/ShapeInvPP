c2461437f2e1ee7e9d30b9f44c365fd7ceda7be4
Zitong Zhang <zztzhang@ucdavis.edu>
2023-12-17 11:27:24 -0800

    SHAP-91 Change max-obs-wise time shifts from 0.05 to 0.2 when x-axis is N_subj and method is 'N_obs=2'.

--- main_run_sim_compare_ShapeInvPP_Nclus1.R
+++ main_run_sim_compare_ShapeInvPP_Nclus1.R
@@ -43,7 +43,7 @@ timeshift_subj_max_vec_list = list(c(1/32/4, 1/32)*2 )
 N_subj_list = list(10,20,30,40,50,60,70,80,90,100)
 key_times_vec_list = list(c(-1,0-0.2,1.5), c(-1,0.04-0.2,1.5), c(-1,0.08-0.2,1.5),
                           c(-1,0.12-0.2,1.5), c(-1,0.16-0.2,1.5), c(-1,0.2-0.2,1.5))
-if (TRUE) {
+if (FALSE) {
   for (timeshift_trial_max in timeshift_trial_max_list){
     default_setting = paste0("timeshift_trial_max=",timeshift_trial_max,",", 
                              'N_spks_total=150,N_subj=10,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2')
@@ -93,59 +93,63 @@ if (TRUE) {
       }
     }
   }
-  
-  for (use_true_timeshift in c(FALSE, TRUE)){
-    default_setting = paste0("use_true_timeshift_", use_true_timeshift,",", 
-                             'N_spks_total=150,N_subj=10,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2')
-    for (id_N_split in 1:N_split) {
-      if (save_res_details & (id_N_split == 1)) {
-        save_center_pdf_array = TRUE
-      } else {
-        save_center_pdf_array = FALSE
-      }
-      for (id_N_subj in 1:length(N_subj_list)) {
-        N_subj = N_subj_list[[id_N_subj]]
-        results <- foreach(j = 1:N_replicate) %dopar% {
-          SEED = sample(1:1e7,1)
-          tryCatch(main_shapeinvpp(SEED = SEED, 
-                                   N_trial = 1,
-                                   N_subj = N_subj,
-                                   N_clus = 1, 
-                                   N_component_true = 2,
-                                   N_spks_total = 150,
-                                   timeshift_subj_max_vec = timeshift_subj_max_vec_list[[1]],
-                                   timeshift_trial_max = 0,
-                                   t_vec = seq(-1,1.5,0.01),
-                                   ### params when N_clus==4:
-                                   clus_sep = 1.4,
-                                   ### Parameters for algorithms
-                                   freq_trun = 10,
-                                   gamma = 1,
-                                   N_component = 2,
-                                   key_times_vec = key_times_vec_list[[1]],
-                                   fix_timeshift = use_true_timeshift,
-                                   use_true_timeshift = use_true_timeshift,
-                                   fix_membership = FALSE,
-                                   save_center_pdf_array = save_center_pdf_array),
-                   error = function(e) print(paste0("SEED = ", SEED, " : ", e)) )
+}
+if (TRUE) {
+  if (FALSE) {
+    for (use_true_timeshift in c(FALSE, TRUE)){
+      default_setting = paste0("use_true_timeshift_", use_true_timeshift,",", 
+                               'N_spks_total=150,N_subj=10,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2')
+      for (id_N_split in 1:N_split) {
+        if (save_res_details & (id_N_split == 1)) {
+          save_center_pdf_array = TRUE
+        } else {
+          save_center_pdf_array = FALSE
         }
-        param_name = "N_subj"
-        param_value = N_subj
-        folder_path = paste0(top_level_folder,
-                             '/', setup,
-                             '/', method, 
-                             '/', default_setting,
-                             '/', param_name, '/', param_value)
-        dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
-        
-        now_replicate = format(Sys.time(), "%Y%m%d_%H%M%S")
-        save(results, file = paste0(folder_path, '/', 'N_replicate', N_replicate, '_', now_replicate, '.Rdata'))
-        rm(results)
+        for (id_N_subj in 1:length(N_subj_list)) {
+          N_subj = N_subj_list[[id_N_subj]]
+          results <- foreach(j = 1:N_replicate) %dopar% {
+            SEED = sample(1:1e7,1)
+            tryCatch(main_shapeinvpp(SEED = SEED, 
+                                     N_trial = 1,
+                                     N_subj = N_subj,
+                                     N_clus = 1, 
+                                     N_component_true = 2,
+                                     N_spks_total = 150,
+                                     timeshift_subj_max_vec = timeshift_subj_max_vec_list[[1]],
+                                     timeshift_trial_max = 0,
+                                     t_vec = seq(-1,1.5,0.01),
+                                     ### params when N_clus==4:
+                                     clus_sep = 1.4,
+                                     ### Parameters for algorithms
+                                     freq_trun = 10,
+                                     gamma = 1,
+                                     N_component = 2,
+                                     key_times_vec = key_times_vec_list[[1]],
+                                     fix_timeshift = use_true_timeshift,
+                                     use_true_timeshift = use_true_timeshift,
+                                     fix_membership = FALSE,
+                                     save_center_pdf_array = save_center_pdf_array),
+                     error = function(e) print(paste0("SEED = ", SEED, " : ", e)) )
+          }
+          param_name = "N_subj"
+          param_value = N_subj
+          folder_path = paste0(top_level_folder,
+                               '/', setup,
+                               '/', method, 
+                               '/', default_setting,
+                               '/', param_name, '/', param_value)
+          dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
+          
+          now_replicate = format(Sys.time(), "%Y%m%d_%H%M%S")
+          save(results, file = paste0(folder_path, '/', 'N_replicate', N_replicate, '_', now_replicate, '.Rdata'))
+          rm(results)
+        }
       }
     }
+    
   }
   if (TRUE) {
-    default_setting = paste0('N_trial=2,N_spks_total=150,N_subj=10,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2')
+    default_setting = paste0('N_trial=2,timeshift_trial_max=0.2,N_spks_total=150,N_subj=10,N_clus=1,clus_sep=1.4,key_time_comp2=-0.2')
     for (id_N_split in 1:N_split) {
       if (save_res_details & (id_N_split == 1)) {
         save_center_pdf_array = TRUE
@@ -163,7 +167,7 @@ if (TRUE) {
                                    N_component_true = 2,
                                    N_spks_total = 150,
                                    timeshift_subj_max_vec = timeshift_subj_max_vec_list[[1]],
-                                   timeshift_trial_max = 0.05,
+                                   timeshift_trial_max = 0.2,
                                    t_vec = seq(-1,1.5,0.01),
                                    ### params when N_clus==4:
                                    clus_sep = 1.4,
@@ -192,10 +196,9 @@ if (TRUE) {
       }
     }
   }
-  
 }
 
-if(TRUE){
+if(FALSE){
   timeshift_trial_max_list = list(0.05, 0.075, 0.1, 0.125, 0.15, 0.175, 0.2)
   timeshift_subj_max_vec_list = list(c(1/32/4, 1/32)*2, c(1/32/4, 1/32)*3,
                                      c(1/32/4, 1/32)*4, c(1/32/4, 1/32)*5,
