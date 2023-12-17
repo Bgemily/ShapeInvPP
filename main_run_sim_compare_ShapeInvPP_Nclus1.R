b6509153372616f07a43b96c487777c3a7d3143c
Zitong Zhang <zztzhang@ucdavis.edu>
2023-12-16 23:11:06 -0800

    SHAP-91 Add replicates

--- main_run_sim_compare_ShapeInvPP_Nclus1.R
+++ main_run_sim_compare_ShapeInvPP_Nclus1.R
@@ -18,8 +18,8 @@ library(parallel)
 
 # User input setup --------------------------------------------------------
 
-N_replicate_total = 20
-N_split = 2
+N_replicate_total = 200
+N_split = 20
 
 N_replicate = N_replicate_total/N_split
 
@@ -33,14 +33,14 @@ doParallel::registerDoParallel(cores = N_cores)
 save_res_details = FALSE
 
 top_level_folder = "../Results/Rdata"
-setup = 'Compare_methods_Nclus1_v3.2.9'
+setup = 'Compare_methods_Nclus1_v3.3'
 method = 'shape_inv_pp'
 
 ### Parameters' possible values:
 timeshift_trial_max_list = list(0, 0.05, 0.1, 0.2)
 N_trial_list = list(1,2,3,4,5,6,7,8,9,10)
 timeshift_subj_max_vec_list = list(c(1/32/4, 1/32)*2 )
-N_subj_list = list(10,30,50,70,90)
+N_subj_list = list(10,20,30,40,50,60,70,80,90,100)
 key_times_vec_list = list(c(-1,0-0.2,1.5), c(-1,0.04-0.2,1.5), c(-1,0.08-0.2,1.5),
                           c(-1,0.12-0.2,1.5), c(-1,0.16-0.2,1.5), c(-1,0.2-0.2,1.5))
 if (TRUE) {
