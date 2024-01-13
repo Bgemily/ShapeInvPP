cfc110dc6ecff4160be9e7ad35035763d6ecfc78
Zitong Zhang <zztzhang@ucdavis.edu>
2024-01-12 23:18:48 -0800

    SHAP-95 Use the same seeds across N_clus_est. Add N_clus_est=6 scenario.

--- main_run_sim_heuristic_gamma.R
+++ main_run_sim_heuristic_gamma.R
@@ -33,12 +33,12 @@ registerDoParallel(cores=N_cores)
 save_res_details = FALSE
 
 top_level_folder = "../Results/Rdata"
-setup = 'heuristic_gamma_v1.2'
+setup = 'heuristic_gamma_v1.3'
 method = 'ShapeInvPP'
 
 ### Parameters' possible values:
 gamma_vec = 10^c(-2,-1.5,-1,-0.5,0,0.5,1,1.5,2)/100
-
+SEED_0 = (as.numeric(format(Sys.time(), "%OS4"))*10^4)*1000
 if (TRUE) {
   default_setting = 'N_clus_est=4,N_trial=3,timeshift_trial_max=0.3,N_spks_total=150,N_subj=40,N_clus=4,clus_sep=0.5,key_time_comp2=-0.2'
   for (id_N_split in 1:N_split) {
@@ -50,7 +50,7 @@ if (TRUE) {
     for (id_gamma in 1:length(gamma_vec)){
       gamma = gamma_vec[id_gamma]
       results <- foreach(j = 1:N_replicate) %dopar% {
-        SEED = sample(1:1e7,1)
+        SEED = SEED_0 + (id_N_split-1)*N_replicate + j
         tryCatch(main_shapeinvpp(SEED = SEED,
                                  N_trial = 3,
                                  N_subj = 40,
@@ -98,7 +98,7 @@ if (TRUE) {
     for (id_gamma in 1:length(gamma_vec)){
       gamma = gamma_vec[id_gamma]
       results <- foreach(j = 1:N_replicate) %dopar% {
-        SEED = sample(1:1e7,1)
+        SEED = SEED_0 + (id_N_split-1)*N_replicate + j
         tryCatch(main_shapeinvpp(SEED = SEED,
                                  N_trial = 3,
                                  N_subj = 40,
@@ -147,7 +147,7 @@ if (TRUE) {
     for (id_gamma in 1:length(gamma_vec)){
       gamma = gamma_vec[id_gamma]
       results <- foreach(j = 1:N_replicate) %dopar% {
-        SEED = sample(1:1e7,1)
+        SEED = SEED_0 + (id_N_split-1)*N_replicate + j
         tryCatch(main_shapeinvpp(SEED = SEED,
                                  N_trial = 3,
                                  N_subj = 40,
@@ -161,6 +161,55 @@ if (TRUE) {
                                  clus_sep = 0.5,
                                  ### Parameters for algorithms
                                  N_clus_min = 5, N_clus_max = 5,
+                                 freq_trun = 10,
+                                 gamma = gamma,
+                                 N_component = 2,
+                                 key_times_vec = c(-1,0-0.2,1.5),
+                                 fix_timeshift = FALSE,
+                                 fix_membership = FALSE,
+                                 save_center_pdf_array = save_center_pdf_array ),
+                 error = function(e) print(paste0("SEED = ", SEED, " : ", e)) )
+      }
+      param_name = "gamma"
+      param_value = gamma
+      folder_path = paste0(top_level_folder,
+                           '/', setup,
+                           '/', method,
+                           '/', default_setting,
+                           '/', param_name, '/', param_value)
+      dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
+      
+      now_replicate = format(Sys.time(), "%Y%m%d_%H%M%S")
+      save(results, file = paste0(folder_path, '/', 'N_replicate', N_replicate, '_', now_replicate, '.Rdata'))
+      rm(results)
+    }
+  }
+}
+if (TRUE) {
+  default_setting = 'N_clus_est=6,N_trial=3,timeshift_trial_max=0.3,N_spks_total=150,N_subj=40,N_clus=4,clus_sep=0.5,key_time_comp2=-0.2'
+  for (id_N_split in 1:N_split) {
+    if (save_res_details & (id_N_split == 1)) {
+      save_center_pdf_array = TRUE
+    } else {
+      save_center_pdf_array = FALSE
+    }
+    for (id_gamma in 1:length(gamma_vec)){
+      gamma = gamma_vec[id_gamma]
+      results <- foreach(j = 1:N_replicate) %dopar% {
+        SEED = SEED_0 + (id_N_split-1)*N_replicate + j
+        tryCatch(main_shapeinvpp(SEED = SEED,
+                                 N_trial = 3,
+                                 N_subj = 40,
+                                 N_clus = 4,
+                                 N_component_true = 2,
+                                 t_vec = seq(-1, 1.5, by=0.01),
+                                 timeshift_subj_max_vec = c(1/32/4, 1/32)*2,
+                                 timeshift_trial_max = 0.3,
+                                 ### params when N_clus==4:
+                                 N_spks_total = 150,
+                                 clus_sep = 0.5,
+                                 ### Parameters for algorithms
+                                 N_clus_min = 6, N_clus_max = 6,
                                  freq_trun = 10,
                                  gamma = gamma,
                                  N_component = 2,
