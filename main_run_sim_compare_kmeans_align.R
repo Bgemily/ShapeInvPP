24ad8356271457fe2123e29a530be26c7ac3b2d6
Zitong Zhang <zztzhang@ucdavis.edu>
2024-01-16 21:43:17 -0800

    SHAP-98 Add experiments where N_trial changes.

--- main_run_sim_compare_kmeans_align.R
+++ main_run_sim_compare_kmeans_align.R
@@ -19,8 +19,8 @@ library(parallel)
 
 # User input setup --------------------------------------------------------
 
-N_replicate_total = 200
-N_split = 20
+N_replicate_total = 500
+N_split = 50
 
 N_replicate = N_replicate_total/N_split
 
@@ -40,6 +40,7 @@ setup = 'Compare_methods_Nclus4_v4.3.3'
 method = 'kmeans_align_use_intensity_no_smth'
 
 ### Parameters' possible values:
+N_trial_list = list(2,3,4,5,6,7,8,9,10)
 timeshift_subj_max_vec_list = list(c(1/32/4, 1/32)*2, c(1/32/4, 1/32)*3, 
                                    c(1/32/4, 1/32)*4, c(1/32/4, 1/32)*5,
                                    c(1/32/4, 1/32)*6, c(1/32/4, 1/32)*7)
@@ -72,7 +73,6 @@ if (TRUE) {
                                    key_times_vec = key_times_vec_list[[1]],
                                    ### Parameters for algorithms
                                    use_intensity = TRUE,
-                                   bw = 'SJ',
                                    N_component = 1,
                                    save_center_pdf_array = save_center_pdf_array),
                  error = function(e) print(paste0("SEED = ", SEED, " : ", e)) )
@@ -94,3 +94,50 @@ if (TRUE) {
   
   
 }
+
+if (TRUE) {
+  default_setting = 'N_trial=2,timeshift_trial_max=0.1,N_spks_total=150,N_subj=40,N_clus=4,clus_sep=0.5,key_time_comp2=-0.2'
+  for (id_N_split in 1:N_split) {
+    if (save_res_details & (id_N_split == 1)) {
+      save_center_pdf_array = TRUE
+    } else {
+      save_center_pdf_array = FALSE
+    }
+    for (id_N_trial in 1:length(N_trial_list)) {
+      N_trial = N_trial_list[[id_N_trial]]
+      results <- foreach(j = 1:N_replicate) %dopar% {
+        SEED = sample(1:1e7,1)
+        tryCatch(main_kmeans_align(SEED = SEED, 
+                                   N_trial = N_trial, timeshift_trial_max = 0.1,
+                                   N_subj = N_subj_list[[1]],
+                                   N_clus = 4, 
+                                   N_component_true = 2,
+                                   N_spks_total = 150,
+                                   timeshift_subj_max_vec = timeshift_subj_max_vec_list[[1]],
+                                   t_vec = seq(-1,1.5,0.01),
+                                   clus_sep = 0.5,
+                                   key_times_vec = key_times_vec_list[[1]],
+                                   ### Parameters for algorithms
+                                   use_intensity = TRUE,
+                                   N_component = 1,
+                                   save_center_pdf_array = save_center_pdf_array),
+                 error = function(e) print(paste0("SEED = ", SEED, " : ", e)) )
+      }
+      param_name = "N_trial"
+      param_value = N_trial
+      folder_path = paste0(top_level_folder,
+                           '/', setup,
+                           '/', method, 
+                           '/', default_setting,
+                           '/', param_name, '/', param_value)
+      dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
+      
+      now_replicate = format(Sys.time(), "%Y%m%d_%H%M%S")
+      save(results, file = paste0(folder_path, '/', 'N_replicate', N_replicate, '_', now_replicate, '.Rdata'))
+      rm(results)
+    }
+  }
+  
+  
+}
+
