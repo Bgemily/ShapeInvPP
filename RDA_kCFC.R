3a0e3bd4c2e78cf0da030aa7a576a3b6a6547ac0
Zitong Zhang <zztzhang@ucdavis.edu>
2024-02-04 11:09:49 -0800

    IA-103 Apply kCFC to real data (empirical density). Nclus = 3.

--- RDA_kCFC.R
+++ RDA_kCFC.R
@@ -36,15 +36,7 @@ for (id_session in c(13)) {
   if (FALSE) {
     trial_length = max(dat$trial_intervals[id_trial_selected,2] - dat$stim_onset[id_trial_selected]) + 0.2
   } else {
-    trial_length = 2.5
-    if (FALSE) {
-      # Reason for setting trial start as 0.4s prior to stim onset: 
-      # 97.5% trials have more than 0.4s from trial start to stim onset
-      quantile(dat$stim_onset[id_trial_selected]-dat$trial_intervals[id_trial_selected,1],0.025) 
-      # Reason for setting trial end as 2.1s post stim onset:
-      # 97.5% trials have more than 2.1s from stim onset in current trial to stim onset in next trial
-      quantile(dat$stim_onset[id_trial_selected+1]-dat$stim_onset[id_trial_selected],0.025)
-    }
+    trial_length = 3.5
   }
   if (identical(feedback_type, 1)) {
     t_vec = seq(0, trial_length, length.out=200)
@@ -53,36 +45,45 @@ for (id_session in c(13)) {
   }
   
   ### Select neuron-trial pairs
-  trial_start_vec = dat$stim_onset - 0.4
+  trial_start_vec = dat$stim_onset - 0.1
   stim_onset_time_vec = (dat$stim_onset - trial_start_vec)[id_trial_selected]
   gocue_time_vec = (dat$gocue - trial_start_vec)[id_trial_selected]
   reaction_time_vec = (dat$reaction_time - trial_start_vec)[id_trial_selected]
   feedback_time_vec = (dat$feedback_time - trial_start_vec)[id_trial_selected]
   
-  spks_time_mlist = matrix(list(), nrow = N_neuron, ncol = N_trial)
-  for (i in 1:N_neuron) {
-    for (j in 1:N_trial) {
+  spks_time_mlist = matrix(list(), nrow = length(id_neuron_selected), ncol = length(id_trial_selected))
+  for (i in 1:length(id_neuron_selected)) {
+    for (j in 1:length(id_trial_selected)) {
       id_neuron = id_neuron_selected[i]
       id_trial = id_trial_selected[j]
       
       spks_vec = dat$spks_pp[id_neuron, id_trial][[1]]
       if ((id_trial+1) <= ncol(dat$spks_pp)) {
         spks_vec_nexttrial = dat$spks_pp[id_neuron, id_trial+1][[1]]
+        stim_onset_nexttrial = dat$stim_onset[id_trial+1] 
+        spks_vec_nexttrial = spks_vec_nexttrial[spks_vec_nexttrial <= stim_onset_nexttrial]
         spks_vec = c(spks_vec, spks_vec_nexttrial)
-      }
-      if ((id_trial-1) >= 1) {
-        spks_vec_prevtrial = dat$spks_pp[id_neuron, id_trial-1][[1]]
-        spks_vec = c(spks_vec_prevtrial, spks_vec)
       }
       spks_shifted_vec = spks_vec - trial_start_vec[id_trial]
       
-      stim_onset_nexttrial = dat$stim_onset[id_trial+1] - trial_start_vec[id_trial]
-      trial_start_currtrial = dat$trial_intervals[id_trial,1] - trial_start_vec[id_trial]
+      # Augment spikes if next trial's stimuli onset occurs before desired observation end time
+      if ( dat$stim_onset[id_trial+1] < (trial_start_vec[id_trial]+trial_length)  ) {
+        spks_shifted_vec_augmented = spks_shifted_vec
+        length_for_fill = 0.4
+        spks_for_fill = spks_shifted_vec[spks_shifted_vec > (dat$stim_onset[id_trial+1]-trial_start_vec[id_trial]-length_for_fill)]
+        length_to_be_fill = (trial_start_vec[id_trial]+trial_length) - (dat$stim_onset[id_trial+1])
+        N_fill = ceiling(length_to_be_fill / length_for_fill )
+        for (id_fill in 1:N_fill) {
+          spks_shifted_vec_augmented = c(spks_shifted_vec_augmented, spks_for_fill + id_fill * length_for_fill)
+        }
+        spks_shifted_vec_augmented = spks_shifted_vec_augmented[spks_shifted_vec_augmented < trial_length]
+        spks_shifted_vec = spks_shifted_vec_augmented
+      }
+      
       trial_end_time = max(t_vec)
       trial_start_time = min(t_vec)
       spks_shifted_vec = spks_shifted_vec[which( (spks_shifted_vec <= trial_end_time) &
                                                    (spks_shifted_vec >= trial_start_time ) )]
-      
       spks_time_mlist[i, j] = list(spks_shifted_vec)
     }
   }
@@ -102,14 +103,17 @@ for (id_session in c(13)) {
     yList[[id_subj]] = emp_density_vec
     tList[[id_subj]] = t_vec
     
-    N_spks_curr_subj = mean(sapply(spks_time_mlist[id_subj, ], function(list_tmp)length(unlist(list_tmp))))
-    yList[[id_subj]] = N_spks_curr_subj * yList[[id_subj]]
+    if (FALSE) {
+      N_spks_curr_subj = mean(sapply(spks_time_mlist[id_subj, ], function(list_tmp)length(unlist(list_tmp))))
+      yList[[id_subj]] = N_spks_curr_subj * yList[[id_subj]]
+      
+    }
     
   }
   
   # Apply kCFC -------
   if (TRUE) {
-    N_clus = 4
+    N_clus = 3
     N_component = 2
     max_attempts <- 5  # You can adjust the number of maximum attempts as needed
     
@@ -151,8 +155,8 @@ for (id_session in c(13)) {
   
   # Save results ------------------------------------------------------------
   top_level_folder = "../Results/Rdata"
-  setup = 'RDA_v3.1.2'
-  method = 'kCFC'
+  setup = 'RDA_v3.2.7'
+  method = 'kCFC_density'
   default_setting = paste0('Session ', id_session, 
                            ', ', brain_region, 
                            ', scenario_num = ', paste0(scenario_num, collapse = '_'),
