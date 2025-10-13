37c68691ac117ca1cda85bf3a7c67ad4f8a9340a
Zitong Zhang <zitong@pascallsystems.com>
2025-10-12 20:33:32 -0400

    SHAP-175 Do more replicates for testing heuristic Nclus method.

--- main_run_sim_heuristic_Nclus.R
+++ main_run_sim_heuristic_Nclus.R
@@ -17,7 +17,7 @@ library(doParallel)
 
 # User input setup --------------------------------------------------------
 
-N_replicate_total = 200
+N_replicate_total = 2000
 N_split = 20
 
 N_replicate = N_replicate_total/N_split
